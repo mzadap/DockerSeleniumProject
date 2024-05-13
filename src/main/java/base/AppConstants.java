fd316021c5448c9378c5ff7178e97771bf682f6c
mzadap <mzadap@gmail.com>
2024-05-13 11:01:33 +0530

    updated app constant class1

--- src/main/java/base/AppConstants.java
+++ src/main/java/base/AppConstants.java
@@ -2,7 +2,7 @@ package base;
 
 public class AppConstants {
 
-    public static final String browserName = System.getProperty("browserName", "remote_git");
-    public static final String platform = System.getProperty("platform", "local");
+    public static final String browserName = System.getProperty("browserName", "chrome");
+    public static final String platform = System.getProperty("platform", "remote_git");
 
 }
